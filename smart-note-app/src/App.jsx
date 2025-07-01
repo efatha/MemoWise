b2e2eb67dba6c230cc89a4ff1b8258c92d07ce5d
efatha <efathabyamungu4@gmail.com>
2025-07-01 22:35:20 +0300

    Removed the default elements from Reat
    After that removal, I initialized the contents in the react app

--- smart-note-app/src/App.jsx
+++ smart-note-app/src/App.jsx
@@ -8,26 +8,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div class="app-container">
+    <header>
+      <h1>📝 Smart Notes</h1>
+      <button id="theme-toggle" title="Toggle Theme">🌙</button>
+    </header>
+
+    <main>
+      <form id="note-form">
+        <textarea id="note-input" maxlength="300" placeholder="Write something smart..."></textarea>
+        <div class="form-footer">
+          <span id="char-count">0/300</span>
+          <button type="submit">➕ Add Note</button>
+        </div>
+      </form>
+
+      <div id="notes-list"></div>
+    </main>
+  </div>
+
     </>
   )
 }
